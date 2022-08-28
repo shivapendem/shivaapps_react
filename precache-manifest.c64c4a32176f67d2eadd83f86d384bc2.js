2c6d44a47d0ea7399c5c0d4892f418855647b3c7
“shivapendem” <pendem.shiva89@gmail.com>
2022-08-28 12:14:35 +0530

    Updates

---
+++ precache-manifest.c64c4a32176f67d2eadd83f86d384bc2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a48b70411a87507c9e59a6043af5f1",
+    "url": "/shivaapps_react/index.html"
+  },
+  {
+    "revision": "74f143ccf838a8e64688",
+    "url": "/shivaapps_react/static/css/main.8d08381b.chunk.css"
+  },
+  {
+    "revision": "35bccb081e0f769ea0f7",
+    "url": "/shivaapps_react/static/js/2.a26e1d29.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/shivaapps_react/static/js/2.a26e1d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f143ccf838a8e64688",
+    "url": "/shivaapps_react/static/js/main.50a41bc6.chunk.js"
+  },
+  {
+    "revision": "af6aa23419e8fd1f31e2",
+    "url": "/shivaapps_react/static/js/runtime-main.8838b2b1.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/shivaapps_react/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/shivaapps_react/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/shivaapps_react/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/shivaapps_react/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/shivaapps_react/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/shivaapps_react/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/shivaapps_react/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/shivaapps_react/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/shivaapps_react/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/shivaapps_react/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/shivaapps_react/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/shivaapps_react/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/shivaapps_react/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/shivaapps_react/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.8654b2b2a2baf6c358f105ec3756a366.js"
+  "/shivaapps_react/precache-manifest.c64c4a32176f67d2eadd83f86d384bc2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shivaapps_react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
